6c8b1cf29ddc235f1de87ff9bfb59d5ab4c0aabc
Jenny Moon <jenny0moon@gmail.com>
2021-06-08 15:33:15 +0100

    create views directory

--- app.rb
+++ app.rb
@@ -2,8 +2,6 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 get '/cat'do
-  "<div style='border: 3px dashed red'>
-     <img src='https://i.imgur.com/jFaSxym.png'>
-   </div>"
+  erb(:index)
 end
 
